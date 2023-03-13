c3eda39e6f8af17635581305b78c34cbad83f9cd
phoenixjw <phoenixjw9320@gmail.com>
2023-03-13 18:49:45 +0000

    installed heroicons instead of using svg link

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,4 +1,6 @@
-import Image from "next/image"
+import Image from "next/image";
+import { MagnifyingGlassIcon } from '@heroicons/react/24/solid'
+
 function Header() {
     return(
         <div>
@@ -9,19 +11,23 @@ function Header() {
                  height={40} 
                  />
                  <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
-                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" 
-                 className="w-6 h-6 text-gray-600">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-                </svg>
-
+                    <MagnifyingGlassIcon className="w-6 h-6 text-gray-600"/>
                     <input 
                     className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500" 
                     type="text" 
-                    placeholder="Search Facebook" />
+                    placeholder="Search Facebook" 
+                    />
 
                  </div>
             </div>
             {/* Header center */}
+            <div className="flex justify-center flex-grow">
+                <div>
+                    {/* <HeaderIcon Icon={HomeIcon}/> */}
+
+                </div>
+
+            </div>
             {/* Header right */}
         </div>
     )
