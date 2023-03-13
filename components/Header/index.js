dd73013d0d1fe7e8bd3891ca1255b844a48951c3
phoenixjw <phoenixjw9320@gmail.com>
2023-03-13 20:30:19 +0000

    added center items to header component

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
-import { MagnifyingGlassIcon } from '@heroicons/react/24/solid'
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+
+import HeaderIcon from "../Headericon";
 
 function Header() {
     return(
-        <div>
+        <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
             {/* Header left */}
             <div className="flex items-center">
                 <Image src="https://links.papareact.com/5me" 
@@ -13,7 +15,7 @@ function Header() {
                  <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
                     <MagnifyingGlassIcon className="w-6 h-6 text-gray-600"/>
                     <input 
-                    className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500" 
+                    className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink" 
                     type="text" 
                     placeholder="Search Facebook" 
                     />
@@ -22,13 +24,21 @@ function Header() {
             </div>
             {/* Header center */}
             <div className="flex justify-center flex-grow">
-                <div>
-                    {/* <HeaderIcon Icon={HomeIcon}/> */}
+                <div className="flex space-x-6 md:space-x-2">
+                    <HeaderIcon active={true} imgURL="/home-icon.svg"/>
+                    <HeaderIcon imgURL="/flag-icon.svg"/>
+                    <HeaderIcon imgURL="/play-icon.svg"/>
+                    <HeaderIcon imgURL="/shopping-cart-icon.svg"/>
+                    <HeaderIcon imgURL="/group-icon.svg"/>
+                
 
                 </div>
 
             </div>
             {/* Header right */}
+            <div>
+                {/* profile pic */}
+            </div>
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,24 @@
+import Header from '@/components/Header'
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
+
 
 export default function Home() {
   return (
-    <>
+    <div>
       <Head>
         <title>Facebook</title>
-
       </Head>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+
+      <Header/>
+
+      <main>
+        {/* sidebar */}
+        {/* feed */}
+        {/* widgets */}
+      </main>
       
-    </>
+    </div>
 
     
   )
