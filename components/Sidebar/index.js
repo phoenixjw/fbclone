90e145ec1f0c82a5ade206d14a01696e926fb58e
phoenixjw <phoenixjw9320@gmail.com>
2023-03-19 15:31:46 +0000

    imported icons for sidebar

--- components/Sidebar/index.js
+++ components/Sidebar/index.js
@@ -1,6 +1,11 @@
+import { ChevronDownIcon, ShoppingBagIcon, UserGroupIcon } from "@heroicons/react/24/outline";
+import { CalendarIcon, ClockIcon, ComputerDesktopIcon, UsersIcon } from "@heroicons/react/24/outline";
 function Sidebar() {
+
+    const { data: session } = useSession();
     return ( 
-        <div>
+        <div className="p-2 mt-5 max-w-[600px] xl:min-w-[300px]">
+            <SideBarRow/>
 
         </div>
     )
