b198ad2e0acbbdf8c3a8f71e52be01a4ed1630d5
phoenixjw <phoenixjw9320@gmail.com>
2023-03-19 15:07:55 +0000

    added usesession hook to access clients fb data

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,14 +1,18 @@
 import Image from "next/image";
 import { BellIcon, ChatBubbleLeftEllipsisIcon, ChevronDownIcon, MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-
+import { useSession } from "next-auth/react";
 import HeaderIcon from "../Headericon";
+import { signOut } from "next-auth/react";
 
 function Header() {
+    const {data: session} = useSession();
+    
     return(
         <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
             {/* Header left */}
             <div className="flex items-center">
-                <Image src="https://links.papareact.com/5me" 
+                <Image 
+                 src="https://links.papareact.com/5me" 
                  width={40}
                  height={40} 
                  />
@@ -38,7 +42,17 @@ function Header() {
 
             {/* Header right */}
             <div className="flex items-center sm:space-x-2 justify-end">
-                {/* profile pic */}
+            {/* profile pic */}
+            <Image
+            onClick={signOut}
+            className="rounded-full cursor-pointer"
+            src={session.user.image}
+            width="40"
+            height="40"
+            style={{objectFit:"fixed"}}/>
+
+
+
             <p className="whitespace-nowrap font-semibold pr-3">Phoenix W</p>
             <BellIcon className="icon"/>
             <ChatBubbleLeftEllipsisIcon className="icon"/>
