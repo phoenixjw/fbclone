e0ac0727f7edb3b9b8a2b7ac5e0fdc62687eaaed
phoenixjw <phoenixjw9320@gmail.com>
2023-03-21 16:43:24 +0000

    Created sidebar component

--- components/Sidebar/index.js
+++ components/Sidebar/index.js
@@ -1,12 +1,22 @@
 import { ChevronDownIcon, ShoppingBagIcon, UserGroupIcon } from "@heroicons/react/24/outline";
 import { CalendarIcon, ClockIcon, ComputerDesktopIcon, UsersIcon } from "@heroicons/react/24/outline";
+import { useSession } from "next-auth/react";
+import SideBarRow from "./SideBarRow";
 function Sidebar() {
 
-    const { data: session } = useSession();
+    const { data: session, status: loading } = useSession();
     return ( 
         <div className="p-2 mt-5 max-w-[600px] xl:min-w-[300px]">
-            <SideBarRow/>
+        <SideBarRow src={session.user.image} title={session.user.name} />
 
+        <SideBarRow Icon={UsersIcon} title="Friends"/> 
+        <SideBarRow Icon={UserGroupIcon} title="Groups"/>
+        <SideBarRow Icon={ShoppingBagIcon} title="Marketplace"/>
+        <SideBarRow Icon={ComputerDesktopIcon} title="Watch"/>
+        <SideBarRow Icon={CalendarIcon} title="Events"/>
+        <SideBarRow Icon={ClockIcon} title="Memories"/>
+        <SideBarRow Icon={ChevronDownIcon} title="See More"/>
+ 
         </div>
     )
 }
