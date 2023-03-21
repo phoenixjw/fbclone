3fd57d520d42d3f4d0476d772017d0926dd6d6c2
phoenixjw <phoenixjw9320@gmail.com>
2023-03-21 16:47:33 +0000

    Created feed component with plan of layout

---
+++ components/Feed/index.js
@@ -0,0 +1,13 @@
+function Feed() {
+    return(
+        <div>
+            <div>
+                {/* Stories */}
+                {/* Input box */}
+                {/* Posts */}
+            </div>
+
+        </div>
+    )
+}
+export default Feed
