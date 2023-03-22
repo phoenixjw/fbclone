c3b590b35768e2f146f3b11bb4cbacce717bd133
phoenixjw <phoenixjw9320@gmail.com>
2023-03-22 10:09:07 +0000

    Added stories component to feed

--- components/Feed/index.js
+++ components/Feed/index.js
@@ -1,8 +1,10 @@
+import Stories from "./Stories"
+
 function Feed() {
     return(
-        <div>
-            <div>
-                {/* Stories */}
+        <div className="flex-grow h-screen pb-44 pt-6 mr-4 xl:mr-40 overflow-y-auto">
+            <div className="mx-auto max-w-md md:max-w-lg lg:max-w-2xl">
+                <Stories/>
                 {/* Input box */}
                 {/* Posts */}
             </div>
