012de3f77fc338f24b1502d0a759d9c64ebcce42
phoenixjw <phoenixjw9320@gmail.com>
2023-03-22 13:36:27 +0000

    installed and setup firebase config

---
+++ firebase.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyChUDiqxP-JOWh3XybxRLPb2BZkRLpsces",
+    authDomain: "facebook-clone-pj.firebaseapp.com",
+    projectId: "facebook-clone-pj",
+    storageBucket: "facebook-clone-pj.appspot.com",
+    messagingSenderId: "901214680617",
+    appId: "1:901214680617:web:78a3d350766b68fc634e38"
+  };
